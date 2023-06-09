9559af95edac2a531a25035aa45a004e512915a4
Wagner Leandro <wagnerleandro3060@gmail.com>
2023-06-08 21:45:52 -0300

    Primeiro commit

---
+++ ToDo/app/src/main/java/com/example/todo/AdicionarTarefaActivity.java
@@ -0,0 +1,33 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AdicionarTarefaActivity extends AppCompatActivity {
+
+    EditText inputTituto;
+    EditText inputDescricao;
+    Button buttonAddTarefa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+
+        inputTituto = findViewById(R.id.inputTituloTarefa);
+        inputDescricao = findViewById(R.id.inputDescTarefa);
+        buttonAddTarefa = findViewById(R.id.buttonAdicionarTarefa);
+        
+    }
+}
---
+++ ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonMenuAddTarefa;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_main);
+
+        //WIDGET:
+        buttonMenuAddTarefa = findViewById(R.id.buttonMenuAdicionar);
+
+    }
+}
