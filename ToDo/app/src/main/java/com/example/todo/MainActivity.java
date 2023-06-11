683865c7e68f018f01793aaeff96579e13e85d76
Wagner Leandro <wagnerleandro3060@gmail.com>
2023-06-10 23:35:44 -0300

    Adicionado o Scrollview no menu principal e o layout do card das tarefas.

--- ToDo/app/src/main/java/com/example/todo/MainActivity.java
+++ ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -23,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         );
         setContentView(R.layout.activity_main);
 
-        //WIDGET:
-        buttonMenuAddTarefa = findViewById(R.id.buttonMenuAdicionar);
 
+        buttonMenuAddTarefa = findViewById(R.id.buttonMenuAdicionar);
     }
 }
